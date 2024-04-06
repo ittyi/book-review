1c5a740309d7649aa538573fd6b814d938f10716
ittyi <vioiv.k4@gmail.com>
2024-04-07 00:09:52 +0900

    🩹 fix: dir 名変更

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <main>
         <form>
           <p>
-            <label for="email">Enter your example.com email:</label>
+            <label for="email">email:</label>
             <input type="email" id="email" pattern=".+@example\.com" size="30" required></input>
           </p>
           <button type="submit">sign in</button>
