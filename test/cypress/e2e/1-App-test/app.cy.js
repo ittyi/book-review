3d93add68aff6116e49247f27042ee9afe45d5ba
ittyi <vioiv.k4@gmail.com>
2024-04-07 01:58:15 +0900

    ✅ add: App テスト

--- test/cypress/e2e/1-App-test/app.cy.js
+++ test/cypress/e2e/1-App-test/app.cy.js
@@ -1,6 +1,17 @@
 describe('初めてのシナリオ App.cy.tsx', () => {  
     it('email フォームのチェック', () => {
       cy.visit('http://localhost:3000/');
-      cy.get('button')
+
+      cy.url().should('eq', 'http://localhost:3000/')
+
+      cy.get('input').should('be.visible')
+        .should('have.value', '')
+        .type('Hello, World')
+      
+      console.log("cy.get('input'): ", cy.get('input'))
+      cy.get('button').should('be.visible')
+        .click()
+
+      cy.url().should('eq', 'http://localhost:3000/')
     });
   })
