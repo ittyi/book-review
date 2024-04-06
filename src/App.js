e638a7b9ffc8a7a6645c0b1a90ae0efba251c171
ittyi <vioiv.k4@gmail.com>
2024-04-06 23:57:24 +0900

    🩹 add: gitignore, yarn init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
       <header className="">
       </header>
       <main>
-        <input></input>
+        <form>
+          <p>
+            <label for="email">Enter your example.com email:</label>
+            <input type="email" id="email" pattern=".+@example\.com" size="30" required></input>
+          </p>
+          <button type="submit">sign in</button>
+        </form>
       </main>
     </div>
   );
