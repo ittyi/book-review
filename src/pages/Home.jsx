2182d6be27b9c103e8d7e9c8b54c9ef445177973
ittyi <vioiv.k4@gmail.com>
2024-04-11 09:40:30 +0900

    💄 add: scss の導入/class main の大枠指定

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,32 +2,39 @@ import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { Header } from "../components/Header";
 import { url } from "../const";
+import "./book-list.scss";
+import "./main.scss";
 
 export const Home = () => {
-  const [listThread, setListThread] = useState([{}]);
+  const [listBook, setListBook] = useState([{}]);
   const [cookies] = useCookies();
 
   useEffect(() => {
     fetch(url + "/books", {
-        headers: {
-          authorization: `Bearer ${cookies.token}`,
-        },
-      })
+      headers: {
+        authorization: `Bearer ${cookies.token}`,
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
-        setListThread(data);
+        setListBook(data);
       });
   }, [cookies.token]);
 
   return (
     <div className="App">
       <Header />
-      <main>
-        <ul>
-          {listThread.map((thread) => (
-            <li key={String(thread.id)}>{thread.title}</li>
-          ))}
-        </ul>
+      <main className="main">
+        <div className="book-list">
+          <h2 className="book-list__heading">書籍レビュー 一覧</h2>
+          <ul className="book-list__content">
+            {listBook.map((book) => (
+              <li key={String(book.id)} className="book-list__book-title">
+                {book.title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
     </div>
   );
